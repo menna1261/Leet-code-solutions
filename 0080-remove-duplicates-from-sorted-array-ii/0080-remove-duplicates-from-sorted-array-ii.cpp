07f9c2f21f5a9895b5d0061a3783764e1c514e2c
Menna Jaheen <118180052+menna1261@users.noreply.github.com>
2024-08-13 23:51:03 +0300

    Update 0080-remove-duplicates-from-sorted-array-ii.cpp

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -7,7 +7,7 @@ public:
         int index = 2; // we're starting from the 3rd position because the first 2 are always valid
 
         for (int i = 2; i < nums.size(); i++) {
-            if (nums[i] != nums[index - 2]) {   // we need to make sure that the next valid element  isn't                                                duplicated more than twice
+            if (nums[i] != nums[index - 2]) {   // we need to make sure that the next valid element  isn't duplicated more than twice
             nums[index++] = nums[i];
 
         }

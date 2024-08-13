a99b3e9780176fa64f143537ccb24e06bb61f071
Menna Jaheen <118180052+menna1261@users.noreply.github.com>
2024-08-13 23:50:12 +0300

    Time: 0 ms (100.00%), Space: 13.5 MB (55.05%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()<=2){
+            return nums.size();
+        }
+        int index = 2; // we're starting from the 3rd position because the first 2 are always valid
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[index - 2]) {   // we need to make sure that the next valid element  isn't                                                duplicated more than twice
+            nums[index++] = nums[i];
+
+        }
+    }
+    return index;
+    }
+};
